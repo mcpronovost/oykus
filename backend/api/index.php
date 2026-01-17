b5044e5d1f186b0106df65e3f39717b88afdfaab
mcpronovost <mcpronovostkurkiency@gmail.com>
2026-01-17 15:26:59 -0500

    clean

--- backend/api/index.php
+++ backend/api/index.php
@@ -2,7 +2,7 @@
 
 header("Content-Type: application/json; charset=utf-8");
 
-define("JWT_SECRET", $_SERVER["JWT_SECRET"] ?? getenv("JWT_SECRET"));
+define("JWT_SECRET", getenv("HTTP_JWT_SECRET"));
 define("JWT_ISSUER", "oykus");
 define("JWT_EXPIRATION", 3600 * 24 * 30);
 
