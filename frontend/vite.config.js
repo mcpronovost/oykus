cc5b56cff02ad3d2be3756317b07eea08b934565
mcpronovost <mcpronovostkurkiency@gmail.com>
2026-01-18 00:39:25 -0500

    update env

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+  // Manual override
+  const DOMAIN = mode === "development" ? "localhost:8080" : "oykus.ovh";
+  const PROTOCOL = mode === "development" ? "http" : "https";
+  const API = mode === "development" ? "localhost:8080/api/v1" : "oykus.ovh/api/v1";
+
   return {
     plugins: [react()],
+    define: {
+      "import.meta.env.VITE_DOMAIN": JSON.stringify(DOMAIN),
+      "import.meta.env.VITE_PROTOCOL": JSON.stringify(PROTOCOL),
+      "import.meta.env.VITE_API": JSON.stringify(API),
+    },
     build: {
       outDir: "dist",
     },
