f0557c1642bef4df6343635ed8667ac4b184641d
mcpronovost <mcpronovostkurkiency@gmail.com>
2026-01-21 15:25:19 -0500

    handle error and loading

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,19 +3,19 @@ import { Construction } from "lucide-react";
 
 import { api } from "@/services/api";
 import { useTranslation } from "@/services/translation";
-import { OykAvatar, OykCard, OykFeedback, OykGrid, OykHeading, OykLink } from "@/components/common";
+import { OykAlert, OykAvatar, OykFeedback, OykGrid, OykHeading, OykLink, OykLoading } from "@/components/common";
 
 export default function Home() {
   const { t } = useTranslation();
 
-  const [isLoading, setIsLoading] = useState(false);
-  const [hasError, setHasError] = useState(null);
+  const [isLoadingWio, setIsLoadingWio] = useState(false);
+  const [hasErrorWio, setHasErrorWio] = useState(null);
   const [wioUsers, setWioUsers] = useState([]);
   const [wioGuests, setWioGuests] = useState(0);
 
   const fetchWio = async (signal) => {
-    setIsLoading(true);
-    setHasError(null);
+    setIsLoadingWio(true);
+    setHasErrorWio(null);
     try {
       const r = await api.get("/wio/", signal ? { signal } : {});
       if (!r?.ok) throw new Error(r.error || t("An error occurred"));
@@ -23,15 +23,15 @@ export default function Home() {
       setWioGuests(r.guests);
     } catch (e) {
       if (e?.name === "AbortError") return;
-      setHasError(() => ({
-        message: e.message || t("An error occurred")
+      setHasErrorWio(() => ({
+        message: e.message || t("An error occurred"),
       }));
     } finally {
       if (!signal.aborted) {
-        setIsLoading(false);
+        setIsLoadingWio(false);
       }
     }
-  }
+  };
 
   useEffect(() => {
     const controller = new AbortController();
@@ -47,18 +47,45 @@ export default function Home() {
     <section className="oyk-page oyk-home">
       <OykHeading title={t("Home")} />
       <OykGrid>
-        <OykFeedback ghost title={t("Under Construction")} message={t("This page is currently under construction. Please check back later.")} icon={Construction} />
+        <OykFeedback
+          ghost
+          title={t("Under Construction")}
+          message={t("This page is currently under construction. Please check back later.")}
+          icon={Construction}
+        />
       </OykGrid>
       <OykHeading title={t("Who is online?")} />
       <OykGrid>
-        <section style={{ display: "flex", flexWrap: "wrap", alignItems: "center" }}>
-          {wioUsers?.length ? wioUsers.map((u) => (
-            <OykLink key={u.slug} routeName={"users-profile"} params={{ userSlug: u.slug }}>
-              <OykAvatar src={u.avatar} borderSize={6} size={48} />
-            </OykLink>
-          )) : null}
-          {wioGuests ? <OykAvatar abbr={`+${wioGuests}`} bgColor="transparent" fgColor="var(--oyk-core-fg)" borderColor="transparent" size={48} /> : null}
-        </section>
+        {hasErrorWio ? (
+          <OykAlert
+            title={t("An error occurred")}
+            message={t("Unable to access who is online data, check your internet connection or try again later")}
+            variant="danger"
+          />
+        ) : !isLoadingWio ? (
+          <>
+            <section style={{ display: "flex", flexWrap: "wrap", alignItems: "center" }}>
+              {wioUsers?.length
+                ? wioUsers.map((u) => (
+                    <OykLink key={u.slug} routeName={"users-profile"} params={{ userSlug: u.slug }}>
+                      <OykAvatar name={u.name} abbr={u.abbr} src={u.avatar} borderSize={6} size={48} />
+                    </OykLink>
+                  ))
+                : null}
+              {wioGuests ? (
+                <OykAvatar
+                  abbr={`+${wioGuests}`}
+                  bgColor="transparent"
+                  fgColor="var(--oyk-core-fg)"
+                  borderColor="transparent"
+                  size={48}
+                />
+              ) : null}
+            </section>
+          </>
+        ) : (
+          <OykLoading />
+        )}
       </OykGrid>
     </section>
   );
