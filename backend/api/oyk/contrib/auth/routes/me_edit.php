d8b27b7a7dc312fae8004208182e2656c00c7945
mcpronovost <mcpronovostkurkiency@gmail.com>
2026-01-21 11:25:51 -0500

    update slug and abbr if auto and name change

--- backend/api/oyk/contrib/auth/routes/me_edit.php
+++ backend/api/oyk/contrib/auth/routes/me_edit.php
@@ -5,12 +5,13 @@ header("Content-Type: application/json");
 require OYK_PATH."/core/middlewares.php";
 require OYK_PATH."/core/db.php";
 require OYK_PATH."/core/utils/uploaders.php";
+require OYK_PATH."/core/utils/formatters.php";
 
 $authUser = require_auth();
 
 try {
     $qry = $pdo->prepare("
-        SELECT name, slug, abbr, avatar, cover
+        SELECT name, slug, is_slug_auto, abbr, is_abbr_auto, avatar, cover
         FROM users
         WHERE id = :id
         LIMIT 1
@@ -32,6 +33,8 @@ if (!$user) {
 $new_name       = $_POST["name"] ?? $user["name"];
 $new_avatar     = $_FILES["avatar"] ?? null;
 $new_cover      = $_FILES["cover"] ?? null;
+$new_slug       = $user["slug"];
+$new_abbr       = $user["abbr"];
 
 if ($new_avatar) {
     $new_avatar = oyk_save_image($new_avatar, 200, 200, "avatars", $user["slug"], 2);
@@ -41,16 +44,26 @@ if ($new_cover) {
     $new_cover = oyk_save_image($new_cover, 1136, 256, "covers", $user["slug"], 2);
 }
 
+if ($user["is_slug_auto"] && $new_name !== $user["name"]) {
+    $new_slug = get_slug($pdo, $new_name, "users");
+}
+
+if ($user["is_abbr_auto"] && $new_name !== $user["name"]) {
+    $new_abbr = get_abbr($new_name, 3);
+}
+
 try {
     $qry = $pdo->prepare("
         UPDATE users
-        SET name=:name, avatar=:avatar, cover=:cover
+        SET name=:name, avatar=:avatar, cover=:cover, slug=:slug, abbr=:abbr
         WHERE id=:id
     ");
     $qry->execute([
         "name" => $new_name,
         "avatar" => $new_avatar ?? $user["avatar"],
         "cover" => $new_cover ?? $user["cover"],
+        "slug" => $new_slug ?? $user["slug"],
+        "abbr" => $new_abbr ?? $user["abbr"],
         "id" => $authUser["id"]
     ]);
 } catch (Exception $e) {
@@ -70,6 +83,8 @@ if ($user["cover"] && $new_cover) {
 $user["name"] = $new_name;
 $user["avatar"] = $new_avatar ?? $user["avatar"];
 $user["cover"] = $new_cover ?? $user["cover"];
+$user["slug"] = $new_slug ?? $user["slug"];
+$user["abbr"] = $new_abbr ?? $user["abbr"];
 
 echo json_encode([
     "ok" => true,
