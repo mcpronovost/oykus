bbf65704abd0ecbbe16fbd377d5a984fa9955ece
mcpronovost <mcpronovostkurkiency@gmail.com>
2026-01-23 14:04:55 -0500

    update

--- backend/api/oyk/contrib/achievements/utils/earn_achievement.php
+++ backend/api/oyk/contrib/achievements/utils/earn_achievement.php
@@ -1,33 +1,37 @@
 <?php
 
 function earn_achievement ($pdo, $achievementKey, $userId) {
-    $qry = $pdo->prepare("
-        SELECT id FROM achievements
-        WHERE achievement_key = :key
-        LIMIT 1;
-    ");
-    $qry->execute([ "key" => $achievementKey]);
-    $achievement = $qry->fetch();
-
-    if (!$achievement) {
-        $stmt = $pdo->prepare("
-            INSERT INTO achievements (id, achievement_key, title, description, category, goal, period)
-            VALUES (1, 'first_login', 'Welcome', 'Log in for the first time.', 'general', 1, 'one-time')';
+    try {
+        $qry = $pdo->prepare("
+            SELECT id FROM achievements
+            WHERE achievement_key = :key
+            LIMIT 1;
         ");
-        $stmt->execute();
-        $achievement = $stmt->fetch();
-    };
+        $qry->execute([ "key" => $achievementKey]);
+        $achievement = $qry->fetch();
 
-    $qry = $pdo->prepare("
-        INSERT IGNORE INTO achievements_users (achievement_id, user_id, unlocked_at, progress)
-        VALUES (:achievementId, :userId, :unlockedAt, :progress);
-    ");
-    $qry->execute([
-        "achievementId" => $achievement["id"],
-        "userId"        => $userId,
-        "unlockedAt"    => date("Y-m-d H:i:s"),
-        "progress"      => 1
-    ]);
+        if (!$achievement) {
+            $stmt = $pdo->prepare("
+                INSERT INTO achievements (id, achievement_key, title, description, category, goal, period)
+                VALUES (1, 'first_login', 'Welcome', 'Log in for the first time.', 'general', 1, 'one-time')';
+            ");
+            $stmt->execute();
+            $achievement = $stmt->fetch();
+        };
+
+        $qry = $pdo->prepare("
+            INSERT IGNORE INTO achievements_users (achievement_id, user_id, unlocked_at, progress)
+            VALUES (:achievementId, :userId, :unlockedAt, :progress);
+        ");
+        $qry->execute([
+            "achievementId" => $achievement["id"],
+            "userId"        => $userId,
+            "unlockedAt"    => date("Y-m-d H:i:s"),
+            "progress"      => 1
+        ]);
+    } catch (Exception $e) {
+        return;
+    }
 
     return;
 };
