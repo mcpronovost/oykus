0fd57c41d5e13d321029b013f95c60abb7de53ff
mcpronovost <mcpronovostkurkiency@gmail.com>
2026-01-25 14:42:31 -0500

    handle custom tag and subtitle

--- frontend/src/components/ui/Heading.jsx
+++ frontend/src/components/ui/Heading.jsx
@@ -1,9 +1,9 @@
-export default function OykHeading({ title, description, actions, ph = 32 }) {
+export default function OykHeading({ title, description, subtitle, actions, ph = 32, tag: Heading = "h1" }) {
   return (
-    <header className="oyk-heading" style={{ padding: `0 ${ph}px` }}>
+    <header className={`oyk-heading ${subtitle ? "oyk-subtitle" : ""}`} style={{ padding: `0 ${ph}px` }}>
       <div className="oyk-heading-wrapper">
-        <div className="oyk-heading-content">
-          {title && <h1 className="oyk-heading-content-title">{title}</h1>}
+        <div className={`oyk-heading-content ${subtitle ? "oyk-subtitle" : ""}`}>
+          {title && <Heading className={`oyk-heading-content-${subtitle ? "subtitle" : "title"}`}>{title}</Heading>}
           {description && <p className="oyk-heading-content-description">{description}</p>}
         </div>
         {actions && <div className="oyk-heading-actions">{actions}</div>}
