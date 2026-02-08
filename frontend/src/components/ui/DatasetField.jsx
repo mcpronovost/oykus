00d6627cd9e4fdc39660bb23bfeb69de8b19c5d3
mcpronovost <mcpronovostkurkiency@gmail.com>
2026-02-07 22:15:38 -0500

    clean

--- frontend/src/components/ui/DatasetField.jsx
+++ frontend/src/components/ui/DatasetField.jsx
@@ -1,11 +1,11 @@
-export default function OykDatasetField({ term = "-", value = "-", preline = false, children}) {
+export default function OykDatasetField({ term = "-", value = "-", preline = false}) {
   return (
     <>
       <dt className="oyk-dataset-term">
         {term}
       </dt>
       <dd className="oyk-dataset-value" style={{ whiteSpace: preline ? "pre-line" : "pre" }}>
-        {value === null || value === "" ? children || "-" : value}
+        {value === null || value === "" ? "-" : value}
       </dd>
     </>
   );
