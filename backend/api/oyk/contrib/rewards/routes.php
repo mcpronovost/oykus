712f679eb48e819190722fc980111dfe9d75ae88
mcpronovost <mcpronovostkurkiency@gmail.com>
2026-02-21 15:34:00 -0500

    restructuration

--- backend/api/oyk/contrib/rewards/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$api = "/api/v1/rewards";
-$view = OYK . "/contrib/rewards/views/";
-
-Router::get("{$api}/achievements", "{$view}achievements.php");
--- backend/api/oyk/core/scripts/drop.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . "/../db.php";
-
-$qry = $pdo->prepare("
-    DROP TABLE tasks;
-")->execute();
