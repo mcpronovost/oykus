8e6d32c462e6854b26a6ed1c26b9043bec09b662
mcpronovost <mcpronovostkurkiency@gmail.com>
2026-03-11 21:08:35 -0400

    update social media buttons to use a dynamic mapping approach

--- frontend/src/pages/Community/ProfileUser.jsx
+++ frontend/src/pages/Community/ProfileUser.jsx
@@ -62,6 +62,29 @@ export default function CommunityProfile() {
   const [userCountComments, setUserCountComments] = useState(0);
   const [userCountReactions, setUserCountReactions] = useState(0);
 
+  const SOCIALS = [
+    { label: "artstation", icon: SiArtstation },
+    { label: "bluesky", icon: SiBluesky },
+    { label: "carrd", icon: SiCarrd },
+    { label: "deviantart", icon: SiDeviantart },
+    { label: "facebook", icon: SiFacebook },
+    { label: "github", icon: SiGithub },
+    { label: "instagram", icon: SiInstagram },
+    { label: "kofi", icon: SiKofi },
+    { label: "linktree", icon: SiLinktree },
+    { label: "mastodon", icon: SiMastodon },
+    { label: "patreon", icon: SiPatreon },
+    { label: "pinterest", icon: SiPinterest },
+    { label: "reddit", icon: SiReddit },
+    { label: "soundcloud", icon: SiSoundcloud },
+    { label: "spotify", icon: SiSpotify },
+    { label: "steam", icon: SiSteam },
+    { label: "tiktok", icon: SiTiktok },
+    { label: "twitch", icon: SiTwitch },
+    { label: "youtube", icon: SiYoutube },
+    { label: "x", icon: SiX },
+  ];
+
   const fetchUserData = async (signal) => {
     setIsLoading(true);
     setHasError(null);
@@ -231,138 +254,17 @@ export default function CommunityProfile() {
                           {userData.meta_website && (
                             <OykButton icon={Globe} small onClick={() => handleOpenSite(userData.meta_website)} />
                           )}
-                          {userData.meta_socials?.artstation && (
-                            <OykButton
-                              icon={SiArtstation}
-                              small
-                              onClick={() => handleOpenSite(userData.meta_socials.artstation)}
-                            />
-                          )}
-                          {userData.meta_socials?.bluesky && (
-                            <OykButton
-                              icon={SiBluesky}
-                              small
-                              onClick={() => handleOpenSite(userData.meta_socials.bluesky)}
-                            />
-                          )}
-                          {userData.meta_socials?.carrd && (
-                            <OykButton
-                              icon={SiCarrd}
-                              small
-                              onClick={() => handleOpenSite(userData.meta_socials.carrd)}
-                            />
-                          )}
-                          {userData.meta_socials?.deviantart && (
-                            <OykButton
-                              icon={SiDeviantart}
-                              small
-                              onClick={() => handleOpenSite(userData.meta_socials.deviantart)}
-                            />
-                          )}
-                          {userData.meta_socials?.facebook && (
-                            <OykButton
-                              icon={SiFacebook}
-                              small
-                              onClick={() => handleOpenSite(userData.meta_socials.facebook)}
-                            />
-                          )}
-                          {userData.meta_socials?.github && (
-                            <OykButton
-                              icon={SiGithub}
-                              small
-                              onClick={() => handleOpenSite(userData.meta_socials.github)}
-                            />
-                          )}
-                          {userData.meta_socials?.instagram && (
-                            <OykButton
-                              icon={SiInstagram}
-                              small
-                              onClick={() => handleOpenSite(userData.meta_socials.instagram)}
-                            />
-                          )}
-                          {userData.meta_socials?.kofi && (
-                            <OykButton icon={SiKofi} small onClick={() => handleOpenSite(userData.meta_socials.kofi)} />
-                          )}
-                          {userData.meta_socials?.linktree && (
-                            <OykButton
-                              icon={SiLinktree}
-                              small
-                              onClick={() => handleOpenSite(userData.meta_socials.linktree)}
-                            />
-                          )}
-                          {userData.meta_socials?.mastodon && (
-                            <OykButton
-                              icon={SiMastodon}
-                              small
-                              onClick={() => handleOpenSite(userData.meta_socials.mastodon)}
-                            />
-                          )}
-                          {userData.meta_socials?.patreon && (
-                            <OykButton
-                              icon={SiPatreon}
-                              small
-                              onClick={() => handleOpenSite(userData.meta_socials.patreon)}
-                            />
-                          )}
-                          {userData.meta_socials?.pinterest && (
-                            <OykButton
-                              icon={SiPinterest}
-                              small
-                              onClick={() => handleOpenSite(userData.meta_socials.pinterest)}
-                            />
-                          )}
-                          {userData.meta_socials?.reddit && (
-                            <OykButton
-                              icon={SiReddit}
-                              small
-                              onClick={() => handleOpenSite(userData.meta_socials.reddit)}
-                            />
-                          )}
-                          {userData.meta_socials?.soundcloud && (
-                            <OykButton
-                              icon={SiSoundcloud}
-                              small
-                              onClick={() => handleOpenSite(userData.meta_socials.soundcloud)}
-                            />
-                          )}
-                          {userData.meta_socials?.spotify && (
-                            <OykButton
-                              icon={SiSpotify}
-                              small
-                              onClick={() => handleOpenSite(userData.meta_socials.spotify)}
-                            />
-                          )}
-                          {userData.meta_socials?.steam && (
-                            <OykButton
-                              icon={SiSteam}
-                              small
-                              onClick={() => handleOpenSite(userData.meta_socials.steam)}
-                            />
-                          )}
-                          {userData.meta_socials?.tiktok && (
-                            <OykButton
-                              icon={SiTiktok}
-                              small
-                              onClick={() => handleOpenSite(userData.meta_socials.tiktok)}
-                            />
-                          )}
-                          {userData.meta_socials?.twitch && (
-                            <OykButton
-                              icon={SiTwitch}
-                              small
-                              onClick={() => handleOpenSite(userData.meta_socials.twitch)}
-                            />
-                          )}
-                          {userData.meta_socials?.youtube && (
-                            <OykButton
-                              icon={SiYoutube}
-                              small
-                              onClick={() => handleOpenSite(userData.meta_socials.youtube)}
-                            />
-                          )}
-                          {userData.meta_socials?.x && (
-                            <OykButton icon={SiX} small onClick={() => handleOpenSite(userData.meta_socials.x)} />
-                          )}
+                          {SOCIALS.map((social) => {
+                            if (!userData.meta_socials[social.label]) return null;
+                            return (
+                              <OykButton
+                                key={social.label}
+                                icon={social.icon}
+                                small
+                                onClick={() => handleOpenSite(userData.meta_socials[social.label])}
+                              />
+                            );
+                          })}
                         </div>
                       ) : null}
                     </section>
