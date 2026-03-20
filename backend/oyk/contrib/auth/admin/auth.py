3d71960ef03566face6149ffe5cb269e20211738
mcpronovost <mcpronovostkurkiency@gmail.com>
2026-03-19 22:25:52 -0400

    add user admin

---
+++ backend/oyk/contrib/auth/admin/auth.py
@@ -0,0 +1,82 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
+from django.utils.translation import gettext_lazy as _, pgettext_lazy
+
+from oyk.contrib.auth.models import OykUser
+
+
+@admin.register(OykUser)
+class OykUserAdmin(UserAdmin):
+    list_display = ("name", "email", "is_active")
+    list_filter = ("is_active",)
+    filter_horizontal = ("groups", "user_permissions")
+    search_fields = ("username", "name", "email")
+    readonly_fields = ("created_at", "updated_at", "last_login")
+
+    fieldsets = (
+        (_("Authentication"), {
+            "fields": ("username", "email", "password")
+        }),
+        (_("Identity"), {
+            "fields": (
+                "name",
+                ("abbr", "is_abbr_auto"),
+                ("slug", "is_slug_auto"),
+            )
+        }),
+        (_("Media"), {
+            "fields": (
+                "avatar",
+                "cover",
+            )
+        }),
+        (_("Preferences"), {
+            "fields": (
+                "timezone",
+            )
+        }),
+        (_("Metadata"), {
+            "fields": (
+                "meta_bio",
+                "meta_birthday",
+                "meta_country",
+                "meta_mood",
+                "meta_website",
+                "meta_socials",
+            )
+        }),
+        (_("Status"), {
+            "fields": (
+                "is_superuser",
+                "is_active",
+                "is_banned",
+            )
+        }),
+        (_("Security"), {
+            "fields": (
+                "banned_until",
+                "banned_reason",
+                "locked_until",
+                "failedlogin_at",
+                "failedlogin_count",
+            )
+        }),
+        (_("Tracking"), {
+            "fields": (
+                "lastlogin_at",
+                "lastlogin_ip",
+                "lastlive_at",
+                "lastlive_ip",
+            )
+        }),
+        (_("Important dates"), {
+            "fields": (
+                "created_at",
+                "updated_at",
+            )
+        }),
+    )
+
+
+admin.site.unregister(Group)
