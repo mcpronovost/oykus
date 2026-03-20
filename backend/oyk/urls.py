979b1bc7c7c0f425e60d6a18955d47734911652b
mcpronovost <mcpronovostkurkiency@gmail.com>
2026-03-19 22:26:07 -0400

    add missing media urls

--- backend/oyk/urls.py
+++ backend/oyk/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
@@ -17,3 +19,12 @@ urlpatterns = [
     path("api/health/", health),
     path("api/heartbeat/", heartbeat),
 ]
+
+urlpatterns += static(
+    settings.STATIC_URL,
+    document_root=settings.STATIC_ROOT
+)
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
