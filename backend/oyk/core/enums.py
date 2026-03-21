ed556346b86eded0d3564cfce580a18b626e5fd3
mcpronovost <mcpronovostkurkiency@gmail.com>
2026-03-20 22:05:14 -0400

    add core roles and visbility enums

---
+++ backend/oyk/core/enums.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class OykRoles(models.IntegerChoices):
+    """User roles"""
+    OWNER = 1, _("Owner")
+    ADMIN = 2, _("Administrator")
+    MODO = 3, _("Moderator")
+    MEMBER = 4, _("Member")
+    VISITOR = 5, _("Visitor")
+    PUBLIC = 6, _("Public")
+
+
+class OykVisibility(models.IntegerChoices):
+    """Visibility levels (higher = more open)."""
+    OWNER = 1, _("Owner")
+    ADMINS = 2, _("Administrators")
+    MODOS = 3, _("Moderators")
+    MEMBERS = 4, _("Members")
+    VISITORS = 5, _("Visitors")
+    PUBLIC = 6, _("Public")
