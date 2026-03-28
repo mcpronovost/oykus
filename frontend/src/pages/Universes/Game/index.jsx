cc074ec0a70154516435c8573ae10c5f4ee1f90b
mcpronovost <mcpronovostkurkiency@gmail.com>
2026-03-28 00:59:25 -0400

    Add game page styles and update Game component structure

--- frontend/src/pages/Universes/Game/index.jsx
+++ frontend/src/pages/Universes/Game/index.jsx
@@ -7,7 +7,7 @@ import { useTranslation } from "@/services/translation";
 import { useWorld } from "@/services/world";
 
 import AppNotAuthorized from "@/components/core/AppNotAuthorized";
-import { OykAlert, OykBanner, OykCard, OykGrid, OykHeading } from "@/components/ui";
+import { OykAlert, OykBanner, OykCard, OykGrid, OykGridRow, OykGridCol, OykHeading, OykLink } from "@/components/ui";
 
 export default function OykUniverseGame() {
   const { isAuth } = useAuth();
@@ -33,22 +33,40 @@ export default function OykUniverseGame() {
 
   return (
     <section className="oyk-game">
-      <header>
-        <OykGrid>
+      <header id="oyk-game-header">
+        <OykGrid id="oyk-game-header-wrapper">
           <OykCard nop fullCenter>
-            <OykBanner
-              height={328}
-              avatarSrc={currentUniverse.logo}
-              avatarSize={128}
-              avatarBorderRadius="4px"
-              avatarTop={180}
-              coverSrc={currentUniverse.cover}
-              coverHeight={256}
-            />
-            <h1>{currentUniverse.name}</h1>
+            <OykBanner height={256} showAvatar={false} coverSrc={currentUniverse.cover} coverHeight={256} />
           </OykCard>
         </OykGrid>
+        <h1 id="oyk-game-header-logo">
+          <OykLink routeName="universe-game" params={{ universeSlug: currentUniverse.slug }}>
+            {currentUniverse.name}
+          </OykLink>
+        </h1>
+        {currentUniverse.tagline && (<p>{currentUniverse.tagline}</p>)}
       </header>
+      <section>
+        <OykGrid>
+          <OykGridRow wrap>
+            <OykGridCol>
+              <OykCard>aaa</OykCard>
+            </OykGridCol>
+            <OykGridCol col="25">
+              <OykCard>bbb</OykCard>
+            </OykGridCol>
+            <OykGridCol col="25">
+              <OykCard>ccc</OykCard>
+            </OykGridCol>
+            <OykGridCol col="25">
+              <OykCard>ddd</OykCard>
+            </OykGridCol>
+            <OykGridCol col="25">
+              <OykCard>eee</OykCard>
+            </OykGridCol>
+          </OykGridRow>
+        </OykGrid>
+      </section>
     </section>
   );
 }
