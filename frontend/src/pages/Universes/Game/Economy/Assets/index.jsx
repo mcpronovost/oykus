6a3413e77ca93fed5dfcf8370a1747378f6853bd
mcpronovost <mcpronovostkurkiency@gmail.com>
2026-04-05 21:46:30 -0400

    update translation

--- frontend/src/pages/Universes/Game/Economy/Assets/index.jsx
+++ frontend/src/pages/Universes/Game/Economy/Assets/index.jsx
@@ -30,8 +30,8 @@ export default function OykUniverseGameEconomyAssets() {
   const menu = [
     {
       id: "assets",
-      title: t("Vos actifs"),
-      description: t("Change identity, logo, description, and more"),
+      title: t("Your Assets"),
+      description: t("Manage your operations, productions, and logistical capacities"),
       Icon: Wallet,
       links: [
         {
@@ -45,30 +45,34 @@ export default function OykUniverseGameEconomyAssets() {
           params: { universeSlug: params?.universeSlug },
         },
         {
-          name: t("Installations"),
-          routeName: "universe-game-economy-assets-facilities",
+          name: t("Productions"),
+          routeName: "universe-game-economy-assets-productions",
           params: { universeSlug: params?.universeSlug },
+          disabled: true,
         },
         {
           name: t("Services"),
           routeName: "universe-game-economy-assets-services",
           params: { universeSlug: params?.universeSlug },
+          disabled: true,
         },
         {
-          name: t("Entrepôts"),
-          routeName: "universe-game-economy-assets-stocks",
+          name: t("Stockages"),
+          routeName: "universe-game-economy-assets-storages",
           params: { universeSlug: params?.universeSlug },
+          disabled: true,
         },
         {
-          name: t("Flux"),
-          routeName: "universe-game-economy-assets-flux",
+          name: t("Transits"),
+          routeName: "universe-game-economy-assets-transits",
           params: { universeSlug: params?.universeSlug },
+          disabled: true,
         },
       ],
     },
   ];
 
-  const exploitations = [
+  const operations = [
     {
       id: 1,
       name: "Mine du domaine Kur",
@@ -124,14 +128,18 @@ export default function OykUniverseGameEconomyAssets() {
           <OykGridMain>
             <section>
               <OykGridRow wrap>
-                {exploitations.map((item) => (<OykGridCol col="33" xl="50" lg="100">
-                  <OykCard>
-                    <h3>{item.name}</h3>
-                    <small>{item.location}</small>
-                    <hr />
-                    <p>Total : {item.total} | Restant : {item.remains}</p>
-                  </OykCard>
-                </OykGridCol>))}
+                {operations.map((item) => (
+                  <OykGridCol col="33" xl="50" lg="100">
+                    <OykCard>
+                      <h3>{item.name}</h3>
+                      <small>{item.location}</small>
+                      <hr />
+                      <p>
+                        Total : {item.total} | Restant : {item.remains}
+                      </p>
+                    </OykCard>
+                  </OykGridCol>
+                ))}
               </OykGridRow>
             </section>
           </OykGridMain>
